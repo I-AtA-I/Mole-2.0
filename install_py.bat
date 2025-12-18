297afda89eb4f1e4084477b203fa1ca4b66c6c18
Vít Sedláček <vitsedl@seznam.cz>
2025-12-18 11:25:18 +0100

    Create install_py.bat

---
+++ install_py.bat
@@ -0,0 +1,8 @@
+@echo off
+winget install python3 --accept-source-agreements --accept-package-agreements
+python -m pip install pycryptodome
+python -m pip install pywin32
+python -m pip install pypiwin32
+
+start cmd /k
+exit
