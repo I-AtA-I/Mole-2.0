1e995d62bf103f2744e41cc9f14abe0fdc473256
Vít Sedláček <vitsedl@seznam.cz>
2026-01-11 20:47:24 +0100

    Update install_py.bat to install Python and Wireshark
    Added installation commands for Python and Wireshark.

--- install_py.bat
+++ install_py.bat
@@ -1,9 +1,12 @@
 @echo off
 
 winget install python3 --accept-source-agreements --accept-package-agreements
+python main.py
 python -m pip install pycryptodome
 python -m pip install pywin32
 python -m pip install pypiwin32
+winget install Wireshark --accept-source-agreements --accept-package-agreements
+
 
 start cmd /k
 exit
