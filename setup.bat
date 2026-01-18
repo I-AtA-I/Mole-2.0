b925524c72966a8f365b88e3f15930eec060e3e5
Vít Sedláček <vitsedl@seznam.cz>
2026-01-18 19:05:13 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -6,6 +6,15 @@ python -m pip install pywin32
 python -m pip install pypiwin32
 winget install Wireshark --accept-source-agreements --accept-package-agreements
 
+python -m pip install colorama
+python -m pip install pynput
+python -m pip install opencv-python
+python -m pip install numpy
+python -m pip install sounddevice
+python -m pip install soundfile
+python -m pip install psutil
+python -m pip install mss
+
 python main.py
 
 start cmd /k
